c49b2132b111c725f8b52012c5e4952daab30b62
AaronRuben <aaronpfennig@gmail.com>
2026-08-18 11:31:17 -0400

    supporting script for evaluating phasing accuracy

--- benchmarking/evaluate_phasing.py
+++ benchmarking/evaluate_phasing.py
@@ -27,6 +27,7 @@ def load_phasing(vcf_f, chrom):
             alt = [a.upper() for a in variant.ALT]
             gt = variant.genotypes[0][:2]
             is_phased = variant.genotypes[0][2]
+            breakpoint()
             ps = variant.FORMAT['PS'] if 'PS' in variant.FORMAT else np.nan
             if len(alt) > 1:
                 raise ValueError("Multi-allelic variants are not supported, "
