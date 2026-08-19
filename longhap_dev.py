45db2639b65a85b20e7a7167459018271444b0fb
AaronRuben <aaronpfennig@gmail.com>
2026-08-19 11:09:42 -0400

    fixed methylation based haplotagging to use llr ratio of 3

--- longhap_dev.py
+++ longhap_dev.py
@@ -1004,8 +1004,8 @@ class LongHap:
             # methylated sites must have a probability greater than 0.5 and an LLR > 3
             meth_states_hap1 = np.zeros_like(meth_hap1) - 1
             meth_states_hap1 = np.where(meth_hap1 - unmeth_hap1 > 3, 1, meth_states_hap1)
-            # unmethylated sites must have a probability greater than 0.5 and an LLR < -3
-            meth_states_hap1 = np.where(unmeth_hap1 - meth_hap1 > 3, 0, meth_states_hap1)
+            # unmethylated sites are all other sites for which methylation information is available
+            meth_states_hap1 = np.where(unmeth_hap1 < 0, 0, meth_states_hap1)
 
             meth_hap2 = methylation_probs[reads_hap2].sum(axis=0)
             unmeth_hap2 = unmethylated_probs[reads_hap2].sum(axis=0)
@@ -1013,9 +1013,9 @@ class LongHap:
             # methylated sites must have a probability greater than 0.5 and an LLR > 3
             meth_states_hap2 = np.zeros_like(meth_hap2) - 1
             meth_states_hap2 = np.where(meth_hap2 - unmeth_hap2 > 3, 1, meth_states_hap2)
-            # unmethylated sites must have a probability greater than 0.5 and an LLR < -3
-            meth_states_hap2 = np.where(unmeth_hap2 - meth_hap2 > 3, 0, meth_states_hap2)
-            breakpoint()
+            # unmethylated sites are all other sites for which methylation information is available
+            meth_states_hap2 = np.where(unmeth_hap2 < 0, 0, meth_states_hap2)
+
             # find differentially methylated sites
             diff_meth = np.where((meth_states_hap1 != meth_states_hap2) & # hap1 and hap2 must have different state
                                  (meth_states_hap1 >= 0) &  # hap1 must be defined
